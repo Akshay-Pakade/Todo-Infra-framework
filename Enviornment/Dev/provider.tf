4ce5f076e8ed80a384b7e1492fecad1a3d6de9bc
Akshay Pakade <pakadeakshay@gmail.com>
2025-11-29 20:43:13 +0530

    Update provider.tf

--- Enviornment/Dev/provider.tf
+++ Enviornment/Dev/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.49.0"
     }
   }
+backend "azurerm" {
+resource_group_name = "dev-rg"
+storage_account_name = "todotv"
+container_name = "tttt"
+key = "terraform.tfstate"
+}
 }
 
 provider "azurerm" {
